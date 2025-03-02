6c4fe30b7e659ee3c3394b000cfe11772ce1c3c5
Dolphin-Syndrom <manvithbhandari01@gmail.com>
2025-03-02 22:44:30 +0530

    deleted anotha

--- models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class TodoItem(BaseModel):
-    id: int
-    title: str
-    description: Optional[str] = None
-    completed: bool = False
